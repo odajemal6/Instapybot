6c08be2cd2519ad2db60d6e1283dbc60af8e27a4
Solo <odajemal2018@outlook.com>
2021-01-25 20:21:47 +0300

    Updated liking capability

--- test.py
+++ test.py
@@ -13,44 +13,30 @@ comments = [
     'Like your post',
     'awesome'
 ]
-
-def follow():
-    session.set_relationship_bounds(enabled=True, potency_ratio=None, delimit_by_numbers=True, max_followers=1000, max_following=900, min_followers=100, min_following=100)
-    session.follow_by_tags([
-        'nazret', 'nazreth', 'nazrēt', 'habeshakonjo', 'habeshawit', 'followforfollowback', 'python_coder2000', 'python_developer', 'python_proglib', 'djangoframework', 'oromo', 'habeshavibe'
-    ], amount=1
-    )
-    
-    
-    session.set_user_interact(amount=3,
-                 percentage=32,
-                  randomize=True,
-                   media='Video')
-    session.follow_commenters(['liya__solomon', 'mafihela', 'asset_east', 'kidus_gech', 'eya_ye_hena'], amount=2, daysold=365, max_pic = 100, sleep_delay=600, interact=True)
-
-def interact():
-    session.set_user_interact(amount=3, randomize=True, percentage=50, media='Photo')
-    session.set_do_follow(enabled=True, percentage=35)
-    session.set_do_like(enabled=True, percentage=70)
-    session.set_comments([comments])
-    session.set_do_comment(enabled=True, percentage=80)
-    session.interact_user_followers(['robsan_hmt'], amount=10, randomize=True)
-
-def likes():
-    session.like_by_tags([
-        'nazret', 'nazreth', 'nazrēt', 'habeshakonjo', 'habeshawit', 'followforfollowback', 'python_coder2000', 'python_developer', 'python_proglib', 'djangoframework'
-    ], amount=10)
-    
-    
-    
-
-def unfollow():
-    session.unfollow_users(amount=20, nonFollowers=True, style="RANDOM", unfollow_after=42*60*60, sleep_delay=655)
-
-
-
+tags = ['habeshabeauty', 'habesha', 'ethiopia', 'ethiopian', 'eth', 
+        'habeshastyle', 'habeshaparty', 'reviewethio', 'habeshafashion',
+        'ethiopianstyle', 'habeshakonjo', 'habeshan', 'habeshanbeauty',
+        'habeshaladies'
+        ]
+location = [
+        '1803190983301422/haile-hotels-and-resorts/',
+        '292006909/nazareth-shewa-ethiopia/',
+        '598461275/adama-shewa-ethiopia/',
+        '220596416/addis-ababa-ethiopia/'
+        ]
 with smart_run(session):
-    #likes()
-    #interact()
-    follow()
-
+    # General setting
+    session.set_do_like(enabled=True, percentage=70)
+    session.set_relationship_bounds(enabled=True,
+                 potency_ratio=1.34,
+                  delimit_by_numbers=True,
+                   max_followers=900,
+                    max_following=4490,
+                     min_followers=100,
+                      min_following=300,
+                       min_posts=0,
+                max_posts=1000)
+    # Liking
+    # Like posts based on hashtags
+    session.like_by_tags(tags, amount=1)
+    session.like_by_locations(location, amount=1, randomize=True)
